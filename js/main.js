9d571ad7b603bb1ffd0dad2beed356c17bd221b4
pc-rishi <pcrishi61@gmail.com>
2024-08-10 20:12:41 +0530

    Chart commit

--- js/main.js
+++ js/main.js
@@ -26,7 +26,6 @@ let cntr = ["152.42905811645483","-27.13675120820229"]
         })
     );
 
-    
     // add fullscreen button
     map.addControl(new mapboxgl.FullscreenControl());
 
@@ -40,9 +39,6 @@ let cntr = ["152.42905811645483","-27.13675120820229"]
         // unit: 'nautical'
     }));
 
-    // map.boxZoom.disable();
-
-
     //Add geolocate
     map.addControl(new mapboxgl.GeolocateControl({
         positionOptions:{
@@ -54,17 +50,17 @@ let cntr = ["152.42905811645483","-27.13675120820229"]
     function addSource() {    
     map.addSource('my-dataset', {
           type: 'vector',
-          url: 'mapbox://rishi13.ao5gc0ra' // Replace with your tileset ID
+          url: 'mapbox://rishi13.ao5gc0ra' 
         });
     
     map.addSource('suburb-bound', {
         type: 'vector',
-        url: 'mapbox://rishi13.42wigdcb' // Replace with your tileset ID
+        url: 'mapbox://rishi13.kryptonite' 
         });
 
     map.addSource('lga-bound', {
         type: 'vector',
-        url: 'mapbox://rishi13.3f21sqvz' // Replace with your tileset ID
+        url: 'mapbox://rishi13.3f21sqvz' 
         });    
     }
     
@@ -72,31 +68,15 @@ let cntr = ["152.42905811645483","-27.13675120820229"]
         function addLayer() {
 
         map.addLayer({
-            'id': 'lga-layer',
-            'type': 'fill',
-            'source': 'lga-bound',
-            'source-layer': 'Somerset_LGA-9vioh7', // Replace with your source layer name
-            'paint': {
-                
-                // 'fill-color': '#007bff',
-                'fill-opacity':0.08,
-                // 'fill-outline-color':'#333',
-                // 'line-width':0.28
-                } 
-            
-        });
-
-        map.addLayer({
             'id': 'suburb-layer',
-            'type': 'line',
+            'type': 'fill',
             'source': 'suburb-bound',
-            'source-layer': 'Somerset_Suburbs-9ljsoa', // Replace with your source layer name
+            'source-layer': 'my-layer', 
             'paint': {
                 
-                // 'fill-color': '#007bff',
-                'line-opacity':0.2,
-                // 'fill-outline-color':'#333',
-                'line-width':0.28
+                'fill-opacity':0.008,
+                'fill-color':'#808080',
+                'fill-outline-color':'#000000'
                 } 
             
         });
@@ -105,7 +85,7 @@ let cntr = ["152.42905811645483","-27.13675120820229"]
           'id': 'dataset-layer',
           'type': 'circle',
           'source': 'my-dataset',
-          'source-layer': 'Somerset_Audit_geocode-8tnjtg', // Replace with your source layer name
+          'source-layer': 'Somerset_Audit_geocode-8tnjtg', 
           'paint': {
             'circle-radius': 2.8,
             'circle-color': '#0078FF',
@@ -161,7 +141,6 @@ let cntr = ["152.42905811645483","-27.13675120820229"]
 
 
 
-
             // Change the cursor to a pointer when over the dataset layer
     map.on('mouseenter', 'dataset-layer', function () {
         map.getCanvas().style.cursor = 'pointer';
@@ -178,7 +157,186 @@ let cntr = ["152.42905811645483","-27.13675120820229"]
       map.on('style.load', function() {
         addSource();
         addLayer();
+
+        
+
+        // Initialize and configure the chart
+        const ctx = document.getElementById('chart').getContext('2d');
+        const chart = new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: [],
+            datasets: [
+            {
+                label: 'Percentage',
+                data: [],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+            ],
+        },
+        options: {
+        indexAxis: 'y',
+        responsive: true,
+        scales: {
+            x: {
+            display: true,
+            title: {
+                display: true,
+                text: 'Percentage',
+            },
+            },
+            y: {
+            display: true,
+            title: {
+                display: true,
+                text: 'Month',
+            },
+            grid: {
+                display: false,
+            },
+            },
+        },
+        plugins: {
+            legend:{
+            display:false
+            },
+            datalabels: {
+            anchor: 'end',
+            align: 'end',
+            display: 'auto',
+            font: {
+                weight: 'bold',
+            },
+            },
+        },
+        },
+
+        });
+
+
+        function updateChart(data) {
+        // Extract the months and values from the filtered data
+        const months = data.map((row) => row.month);
+        const values = data.map((row) => row.value);
+
+        // Update the chart with the new data
+        chart.data.labels = months;
+        chart.data.datasets[0].data = values;
+        chart.options.layout = {
+            padding: {
+            top: 10, // Adjust the value to move the chart lower
+            right: 0,
+            bottom: 0,
+            left: 0
+            }
+        };
+
+        const selectedSa3 = data[0].Suburb_Name; // Get the selected sa3 value from the first data point
+        chart.options.plugins.title = {
+            display: true,
+            text: selectedSa3, // Set the selected sa3 value as the chart title
+            position: 'top',
+            font: {
+            size: 15 // Adjust the value to increase or decrease the font size
+            },
+            color: '#333',
+            padding: {
+            top: 0,
+            bottom: 10
+            }
+        };
+
+        // Hide the legend
+        chart.options.plugins.legend.display = false;
+
+        chart.update();
+        }
+
+        // Fetch the CSV data
+        fetch('https://raw.githubusercontent.com/pc-rishi/mapbox-foundation/main/data/Somerset_Suburbs_Accom.csv')
+        .then((response) => response.text())
+        .then((csvData) => {
+        // Step 1: Parse the CSV data
+        const parsedData = parseCSVData(csvData);
+
+        // Update the chart with initial data for "All" SA3 when the "Total Spend" category is selected
+        // const initialCategory = 'Total Spend';
+        updateChart(
+            parsedData.filter((row) => row.Suburb_Name === 'Laceys Creek')
+        );
+
+        // Click event listener for state-fills layer
+        map.on('click', 'suburb-layer', (e) => {
+            if (e.features.length > 0) {
+            const sa3Value = e.features[0].properties['Suburb_Name'];
+            // const selectedMerchantCategory = filterSelect.value;
+
+            if (sa3Value === 'Laceys Creek') {
+                // If 'All' SA3 is clicked, show data for 'All' SA3
+                updateChart(
+                parsedData.filter(
+                    (row) => row.Suburb_Name === 'Laceys Creek' 
+                )
+                );
+            } else {
+                // Filter the CSV data based on the clicked SA3 value and the selected merchant category
+                const filteredData = parsedData.filter(
+                (row) => row.Suburb_Name === sa3Value 
+                );
+                updateChart(filteredData);
+            }
+            } else {
+            // No feature is clicked, show data for "All" SA3
+            // const selectedMerchantCategory = filterSelect.value;
+            updateChart(
+                parsedData.filter(
+                (row) => row.Suburb_Name === 'Laceys Creek' 
+                )
+            );
+            }
+        });
+
+        // Click event listener for the map (when no feature is clicked)
+        map.on('click', (e) => {
+            if (!map.queryRenderedFeatures(e.point, { layers: ['suburb-layer'] }).length) {
+            // No feature is clicked, show data for "All" SA3
+            // const selectedMerchantCategory = filterSelect.value;
+            updateChart(
+                parsedData.filter(
+                (row) => row.Suburb_Name === 'Laceys Creek' 
+                )
+            );
+            }
+        });
+
+
+        })
+
+
+
+
+
+
+        function parseCSVData(csvData) {
+        const parsedData = Papa.parse(csvData, { header: true }).data;
+        return parsedData.map((row) => ({
+        month: row['month'],
+        value: parseFloat(row['value']),
+        // merch:row['Merchant Category Code'],
+        Suburb_Name:row['Suburb_Name']
+        }));
+        }
+
+
+
+
       });
+
+
+
+      //Switch Layer functionality
   
       function switchLayer(layer) {
         var layerId = layer.target.id;
@@ -190,20 +348,4 @@ let cntr = ["152.42905811645483","-27.13675120820229"]
       }
 
 
-
-// }
-
-// mapBox(cntr);
-
-
-
-// A way to log the current location
-navigator.geolocation.getCurrentPosition(position=>{
-    console.log(position);
-    let lat = position.coords.latitude;
-    let lng = position.coords.longitude;
-    let center = [lng,lat];
-    console.log(center);
-
-    // mapBox(center);
-})
+      
